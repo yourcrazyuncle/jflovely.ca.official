e7f887be98c5f1f44357362097e5bb9dd04579c1
seb <yourcrazyuncle@protonmail.com>
2026-09-11 14:59:30 -0700

    i don't know how this thing works take 18

--- links.js
+++ links.js
@@ -19,5 +19,6 @@ const links = {
     speed: "https://speed.cloudflare.com/",
     speed2: "https://www.speedtest.net/",
     windy: "https://www.windy.com/49.261/-123.114?waves,49.532,-122.574,9",
-    echords: "https://www.e-chords.com/"
+    echords: "https://www.e-chords.com/",
+    github: "https://github.com/login",
 }
